8fc22abe665f6f9c65fc83f4201ce142e77e8675
a1barif2h <mohammadarifhossain80@gmail.com>
2021-06-03 19:26:10 +0600

    drag and drop for table col and row

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+// import DragAndDropColumnsTable from './compnents/DragAndDropColumnsTable';
+import ColAndRowDragAndDrop from './compnents/ColAndRowDragAndDrop';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{textAlign: 'center'}}>Drag and Drop Row in Table</h1>
+      {/* <DragAndDropTable /> */}
+      {/* <DragAndDropColumnsTable /> */}
+      {/* <Demo /> */}
+      <ColAndRowDragAndDrop />
     </div>
   );
 }
